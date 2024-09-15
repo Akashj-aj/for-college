a427a80402561870183253e606a8f4a3b4a5ec9f
paperbukit <paperbukit@outlook.com>
2024-09-15 13:37:32 +0530

    prime or not program (python)

---
+++ python/prime_or_not.py
@@ -0,0 +1,13 @@
+n = int(input("enter the number: "))
+if n == 0 or n == 1:
+    print(n, "is not a prime number")
+elif n>1:
+    for i in range(2,n):
+        if (n % i) == 0:
+            print(n,"is not a prime number ")
+            break
+        else:
+            print(n,"is a prime number")
+            break
+else:
+    print(n,"is not a prime number")
